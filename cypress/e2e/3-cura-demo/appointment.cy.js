78bca61cdd1d56e3135991db7b1793f70415dfa2
anisanursekararum <anisanursekararum@gmail.com>
2023-09-08 15:52:12 +0700

    positive case appointment

---
+++ cypress/e2e/3-cura-demo/appointment.cy.js
@@ -0,0 +1,28 @@
+import loginPage from "../../support/page/loginPage"
+import appointmentPage from "../../support/page/appointmentPage"
+const data = require("../../fixtures/data.json")
+
+describe('Appointment', () => {
+  beforeEach(() => {
+    cy.visit('')
+    loginPage.clickMenu()
+    loginPage.clickLoginMenu()
+    cy.url().should('contain', 'login')
+    loginPage.inputUsername(data.username)
+    loginPage.inputPassword(data.password)
+    loginPage.clickButtton()
+    loginPage.verifyLogin()
+  })
+
+  it('Book Appointment All Form Filled', () => {
+    appointmentPage.selectFacility()
+    appointmentPage.checkCheckbox()
+    appointmentPage.radioHealthCareProgram()
+    appointmentPage.chooseDate()
+    appointmentPage.typeComment()
+    appointmentPage.clickButtonBookApp()
+    appointmentPage.confirmation()
+  })
+
+
+})
---
+++ cypress/support/page/appointmentPage.js
@@ -0,0 +1,45 @@
+const data = require("../../fixtures/data.json")
+
+class appointmentPage {
+  facility = '#combo_facility'
+  checkbox = '#chk_hospotal_readmission'
+  radio = '#radio_program_medicaid'
+  inputDate = '#txt_visit_date'
+  datePicker = '.datepicker-days'
+  date = '.day'
+  comment = '#txt_comment'
+  buttonBookApp = '#btn-book-appointment'
+  titleConfirm = 'h2'
+  
+  selectFacility(){
+    cy.get(this.facility).select(data.facility3).should('have.value', data.facility3)
+  }
+
+  checkCheckbox(){
+    cy.get(this.checkbox).check().should('be.checked')
+  }
+
+  radioHealthCareProgram(){
+    cy.get(this.radio).check(data.program2).should('have.value', data.program2)
+  }
+
+  chooseDate(){
+    cy.get(this.inputDate).click()
+    cy.get(this.datePicker).should('be.visible')
+    cy.get(this.date).contains('30').click()
+  }
+
+  typeComment(){
+    cy.get(this.comment).should('be.visible').type(data.comment)
+  }
+  
+  clickButtonBookApp(){
+    cy.get(this.buttonBookApp).should('be.visible').click()
+  }
+
+  confirmation(){
+    cy.get(this.titleConfirm).should('be.visible').and('have.text', data.titleConfirm)
+  }
+}
+
+export default new appointmentPage()
